ab5fdb575580d1d1c04f0e4dbae8dd1638dbf8d7
Vivian Davila <viviananddav@gmail.com>
2024-04-02 08:17:55 -0700

    Challenge 2 passed

--- javascript/code-challenges/challenges-06.test.js
+++ javascript/code-challenges/challenges-06.test.js
@@ -26,7 +26,7 @@ Write a function named addValues that, given an array of numbers as input, uses 
 ------------------------------------------------------------------------------------------------ */
 
 const addValues = (arr) => {
-  // Solution code here...
+  return arr.reduce((total, current) => total + current, 0);
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -292,7 +292,7 @@ describe('Testing challenge 1', () => {
   });
 });
 
-xdescribe('Testing challenge 2', () => {
+describe('Testing challenge 2', () => {
   test('It should add the values of an array', () => {
     expect(addValues([1, 2, 3, 4, 5])).toStrictEqual(15);
     expect(addValues([])).toStrictEqual(0);
