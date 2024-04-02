ec2a62108ea15e34be4eb31625170c06fb46368a
Vivian Davila <viviananddav@gmail.com>
2024-04-02 08:27:19 -0700

    Challenge 3 passed

--- javascript/code-challenges/challenges-06.test.js
+++ javascript/code-challenges/challenges-06.test.js
@@ -42,7 +42,9 @@ Write a function named addPurchases that, given an array of objects as input, us
 ------------------------------------------------------------------------------------------------ */
 
 const addPurchases = (arr) => {
-  // Solution code here...
+  return arr.reduce((total, purchase) => {
+    return total + purchase.purchasePrice;
+  }, 0);
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -300,7 +302,7 @@ describe('Testing challenge 2', () => {
   });
 });
 
-xdescribe('Testing challenge 3', () => {
+describe('Testing challenge 3', () => {
   test('It should add the purchase price', () => {
     expect(addPurchases([{ item: 'switch', purchasePrice: 399 }, { item: 'toothpaste', purchasePrice: 2 }])).toStrictEqual(401);
     expect(addPurchases([])).toStrictEqual(0);
