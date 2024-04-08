a2d4e1c3f7e932f95cef6e1d5ed3313d561e3dbe
Vivian Davila <viviananddav@gmail.com>
2024-04-08 00:41:08 -0700

    Test 5 passed

--- javascript/code-challenges/challenges-03.test.js
+++ javascript/code-challenges/challenges-03.test.js
@@ -59,7 +59,7 @@ For example, notInFirstArray([1,2,3], [1,2,3,4]) returns [4].
 ------------------------------------------------------------------------------------------------ */
 
 const notInFirstArray = (forbiddenValues, arr) => {
-  // Solution code here...
+  return arr.filter(item => !forbiddenValues.includes(item));
 };
 
 /* ------------------------------------------------------------------------------------------------
