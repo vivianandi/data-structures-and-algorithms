97fa1dcf5c118e949f3becd5e86fe76d032d6c06
Vivian Davila <viviananddav@gmail.com>
2024-04-09 21:41:56 -0700

    Test 4 passed

--- javascript/code-challenges/challenges-12.test.js
+++ javascript/code-challenges/challenges-12.test.js
@@ -72,7 +72,10 @@ For example, [ [0,2,5,4], [2,4,10], [] ] should return [ [1, 32], [1024], [] ].
 ------------------------------------------------------------------------------------------------ */
 
 const divisibleByFiveTwoToThePower = (input) => {
-  // Solution code here...
+  return input.map(arr => {
+    return arr.filter(num => typeof num === 'number' && num % 5 === 0)
+      .map(num => Math.pow(2, num));
+  });
 };
 
 /* ------------------------------------------------------------------------------------------------
