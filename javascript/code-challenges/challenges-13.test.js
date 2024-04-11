cbc250a88ba0884b8513ae03b202eef4b9b421c4
Vivian Davila <viviananddav@gmail.com>
2024-04-10 23:40:58 -0700

    Tests 1, 2, 3, 5, 6 pass, 4 fails

--- javascript/code-challenges/challenges-13.test.js
+++ javascript/code-challenges/challenges-13.test.js
@@ -127,7 +127,8 @@ Write a function named citiesAtoJ that takes in an array of city names and uses 
 ------------------------------------------------------------------------------------------------ */
 
 const citiesAtoJ = (arr) => {
-  // Solution code here...
+  const pattern = /^[A-J]/i;
+  return arr.filter(city => pattern.test(city));
 };
 
 /* ------------------------------------------------------------------------------------------------
