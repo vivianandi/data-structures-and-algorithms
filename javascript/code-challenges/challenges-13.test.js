a3108e7d5fc97dc8f3c4e155b5d801b36a7a99d2
Vivian Davila <viviananddav@gmail.com>
2024-04-10 23:31:29 -0700

    Tests 1-4 passed

--- javascript/code-challenges/challenges-13.test.js
+++ javascript/code-challenges/challenges-13.test.js
@@ -103,7 +103,8 @@ Write a function named containsWorld that takes in a string or number of any len
 ------------------------------------------------------------------------------------------------ */
 
 const containsWorld = (input) => {
-  // Solution code here...
+  const pattern = /\bworld\b/;
+  return pattern.test(input.toString().toLowerCase());
 };
 
 /* ------------------------------------------------------------------------------------------------
